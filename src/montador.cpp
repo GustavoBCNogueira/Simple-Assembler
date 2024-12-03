c0801b5775f900fd08aa3fe8aff8b5f453989d33
Gubsb <nogueiragustavo207@gmail.com>
2024-12-03 18:26:45 -0300

    complementando a primeira passagem

--- src/montador.cpp
+++ src/montador.cpp
@@ -9,12 +9,23 @@ void readFile(ifstream& file) {
     }
 }
 
+void build_tabela_instrucoes(map<string, int>& tabela_instrucoes) {
+    tabela_instrucoes = {{"add", 2}, {"sub", 2}, {"mul", 2},
+                         {"div", 2}, {"jmp", 2}, {"jmpn", 2}, 
+                         {"jmpp", 2}, {"jmpz", 2}, {"copy", 3}, 
+                         {"load", 2}, {"store", 2}, {"input", 2}, 
+                         {"output", 2}, {"stop", 1}};
+}
+
 void primeira_passagem(ifstream& file) {
     int contador_posicao = 0;
     int contador_linha = 1;
     string linha, delimiter = " ";
     map<string, int> tabela_simbolos;
-    map<string, int> tabela_operacoes;
+    map<string, int> tabela_instrucoes;
+    map<string, int> tabela_diretivas = {{"CONST", 1}, {"SPACE", 1}};
+
+    build_tabela_instrucoes(tabela_instrucoes);
 
     while (getline(file, linha)) {
         vector<string> tokens;
@@ -41,11 +52,19 @@ void primeira_passagem(ifstream& file) {
         }
 
         string operacao = tokens[0];
-        if (tabela_operacoes.count(operacao) == 1) {
-            contador_posicao += tabela_operacoes[operacao];
+        
+        if (tabela_instrucoes.count(operacao) == 1) {
+            contador_posicao += tabela_instrucoes[operacao];
         } else {
-
+            if (tabela_diretivas.count(operacao) == 1) {
+                // chama subrotina que executa a tarefa
+                // contador_posicao += retorno da subrotina
+            } else {
+                cout << "Erro! Operação não identificada!\n";
+            }
         }
+
+        contador_linha++;
     }
 }
 
